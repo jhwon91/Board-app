6529d3e76598c9e3a4e88b84e543a67360968cd4
won <jh.won@kisos.net>
2022-05-24 22:07:43 +0900

    start nest js

---
+++ src/boards/boards.controller.ts
@@ -0,0 +1,18 @@
+import { Controller } from '@nestjs/common';
+import { BoardsService } from './boards.service';
+
+@Controller('boards')
+export class BoardsController {
+	// boardsService: BoardsService
+	// constructor( boardsService: BoardsService) {
+	// 	this.boardsService = boardsService
+	// }
+	//=> 접근 제한자 (public, protected, private)을 생성자 파라미터에 선언하면
+	//접근 제한자가 사용된 생성자 파라미터는 암묵적으로 클래스 프로퍼티로 선언이 됨
+
+	constructor(private boardsService: BoardsService) {}
+
+
+
+
+}
